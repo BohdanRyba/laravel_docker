9416638c85765c7db13b70fde3d446982ea1a6c1
AgurSoft <web.dev.asoft@gmail.com>
2018-05-29 12:37:35 +0300

    add laravel debugbar and ide helper

--- _ide_helper.php
+++ _ide_helper.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.6.23 on 2018-05-29 09:30:51.
+ * Generated for Laravel 5.6.23 on 2018-05-29 09:36:28.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
